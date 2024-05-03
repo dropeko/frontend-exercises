9a95a604fda511834645cd5c40e0222d312cb3b3
pedroandrade <pedro.andrade@sillion.com.br>
2024-05-03 14:13:45 -0300

    JS assincrono

---
+++ js-assincrono-promise-fetch/fetchAPI.js
@@ -0,0 +1,17 @@
+
+const divRender = document.getElementById('featured-post-body');
+const divAuthor = document.getElementById('featured-post-title');
+const buttonRenderQuote = document.getElementById('button-fetch');
+
+
+const fetchDummyJson = async () => {
+    const fetchDummy = await fetch('https://dummyjson.com/quotes/random');
+    const fetchDummyJson = await fetchDummy.json();
+    console.log(fetchDummyJson);
+
+    
+    divRender.innerHTML = fetchDummyJson.quote;
+    divAuthor.innerHTML = fetchDummyJson.author;
+};
+
+buttonRenderQuote.addEventListener('click', fetchDummyJson);
